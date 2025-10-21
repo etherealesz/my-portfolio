2f763936070cb500c34f5c4411977dbe357969f5
enardostefanuszx <stefanus.enardo05@gmail.com>
2025-10-21 12:04:03 +0700

    added gh-p dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/Portfolio/',
+    base: '/my-portfolio/',
     plugins: [react()],
     server: {
         port: 3000,
